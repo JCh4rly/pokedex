98226c498af15a2bf610402cec0d1943fad1517b
MrJCharly <carlosepintor@hotmail.com>
2022-07-31 12:03:59 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GET_POKEMONS = gql`
+  query samplePokeAPIquery {
+    pokemon_v2_pokemon(limit: 40) {
+      name
+      height
+      weight
+      order
+      pokemon_v2_pokemonabilities {
+        pokemon_v2_ability {
+          name
+        }
+      }
+      pokemon_v2_pokemontypes {
+        pokemon_v2_type {
+          name
+        }
+      }
+      pokemon_v2_pokemonsprites_aggregate {
+        nodes {
+          sprites
+        }
+      }
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_POKEMONS);
+  const getSprite = (sprites: any) => JSON.parse(sprites?.nodes[0]?.sprites)?.other?.dream_world?.front_default;
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+
+  return <>
+    <ul>
+      {data.pokemon_v2_pokemon.map(({ name, pokemon_v2_pokemonsprites_aggregate: sprites }: any) => <li>
+        {name}
+        <img src={getSprite(sprites)} alt="sprite" width={100} height={100}/>
+      </li>)}
+    </ul>
+  </>;
 }
 
 export default App;

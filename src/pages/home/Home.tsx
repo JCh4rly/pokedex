146b40906d1cf9bbcfedb77bea54e49b54bdeff8
MrJCharly <carlosepintor@hotmail.com>
2022-08-01 12:00:20 -0300

    Added pagination

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { useQuery, gql, NetworkStatus } from '@apollo/client';
+import { Button, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import TypeTag from '../../components/TypeTag';
 
 const GET_POKEMONS = gql`
-  query samplePokeAPIquery {
-    pokemon_v2_pokemon(limit: 40) {
+  query samplePokeAPIquery($offset: Int!) {
+    pokemon_v2_pokemon(limit: 12, offset: $offset) {
       name
       height
       weight
@@ -31,8 +31,25 @@ const GET_POKEMONS = gql`
 `;
 
 const Home = () => {
-  const { loading, error, data } = useQuery(GET_POKEMONS);
+  const rowsPerpage = 12;
+  const { loading, error, data, refetch } = useQuery(GET_POKEMONS, {
+    variables: { offset: 0 }
+  });
+  const [pokemons, setPokemons] = React.useState<any[]>([]);
+  const [page, setPage] = React.useState(0);
   const getSprite = (sprites: any) => JSON.parse(sprites?.nodes[0]?.sprites)?.other?.dream_world?.front_default;
+
+  React.useEffect(() => {
+    if (data) {
+      setPokemons([...pokemons, ...data.pokemon_v2_pokemon]);
+    }
+  }, [data])
+
+  React.useEffect(() => {
+    if (page > 0) {
+      refetch({ offset: rowsPerpage * page });
+    }
+  }, [page]);
 
   if (loading) {
     return <p>Loading...</p>
@@ -42,9 +59,11 @@ const Home = () => {
     return <p>Error: {error.message}</p>
   }
 
+  const loadMoreItems = () => setPage((page) => (page + 1));
+
   return <>
     <Grid container spacing={1}>
-      {data.pokemon_v2_pokemon.map(({ order, name, pokemon_v2_pokemonsprites_aggregate: sprites, pokemon_v2_pokemontypes: types }: any) =>
+      {pokemons.map(({ order, name, pokemon_v2_pokemonsprites_aggregate: sprites, pokemon_v2_pokemontypes: types }: any) =>
       <Grid item xs={6} md={3}>
         <Card sx={{ padding: '5px' }}>
           <CardMedia
@@ -67,6 +86,15 @@ const Home = () => {
           </CardContent>
         </Card>
       </Grid>)}
+      <Grid item xs={12} md={12} sx={{ textAlign: 'center' }}>
+        <Button 
+          variant="contained" 
+          onClick={loadMoreItems}
+          disabled={loading}
+        >
+          Load more Pokemons
+        </Button>
+      </Grid>
     </Grid>
   </>;
 }
